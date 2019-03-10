404249b24da204f3023a8e7606b84c8fa0c7cf9f
guidoAI <guido.de.croon@gmail.com>
2019-03-10 22:11:36 +0100

    Added the notebook, etc.

---
+++ live.py
@@ -0,0 +1,44 @@
+# This script will detect faces via your webcam.
+# Tested with OpenCV3
+# Code adapted from: https://github.com/shantnu/Webcam-Face-Detect
+
+import cv2
+import time
+
+def run_detection():
+    cap = cv2.VideoCapture(0)
+    
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    
+    while(True):
+    	# Capture frame-by-frame
+    	ret, frame = cap.read(); start_time = time.time();
+        
+    	# Our operations on the frame come here
+    	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    	# Detect faces in the image
+    	faces = faceCascade.detectMultiScale(
+    		gray,
+    		scaleFactor=1.1,
+    		minNeighbors=5,
+    		minSize=(30, 30)
+    		#flags = cv2.CV_HAAR_SCALE_IMAGE
+    	); end_time = time.time();
+    
+    	print("Found {0} faces, in {1} seconds.".format(len(faces), end_time - start_time), end='\r')
+    
+    	# Draw a rectangle around the faces
+    	for (x, y, w, h) in faces:
+    		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    
+    	# Display the resulting frame
+    	cv2.imshow('frame', frame)
+    	if cv2.waitKey(1) & 0xFF == ord('q'):
+    		break
+    
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
